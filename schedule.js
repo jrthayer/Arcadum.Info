8f4199b22c204c64b005df06d244a5fc2b7f045b
James Thayer <jrthayer@users.noreply.github.com>
2021-07-08 16:46:05 -0700

    Add ink and blood back to the schedule

--- schedule.js
+++ schedule.js
@@ -11,7 +11,7 @@
 //================================
 var utcTimes = [
     [[1380, "Update Stream!!"]],
-    [[1320, "The Final Toll"]],
+    [[1020, "Ink And Blood"],[1320, "The Final Toll"]],
     [[1260, "A Storm Approaches"]],
     [[1380, "Among The Reeds"]], 
     [[1080, "The Divine Wind"]], 
